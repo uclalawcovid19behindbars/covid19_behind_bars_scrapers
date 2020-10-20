8ba61a738eb23d66efe037ca1bc9c4863977664d
Neal Marquez <nmmarquez@gmail.com>
2020-10-20 10:53:21 -0400

    feat: add NY scrapers

---
+++ production/scrapers/new_york.R
@@ -0,0 +1,96 @@
+source("./R/generic_scraper.R")
+source("./R/utilities.R")
+
+new_york_pull <- function(x){
+    get_src_by_attr(
+        x, "a", attr = "href", attr_regex = "(?i)confirmed-facility")
+}
+
+new_york_restruct <- function(x){
+    ny_pgs <- magick::image_read_pdf(x)
+    ExtractTable(ny_pgs)
+}
+
+new_york_extract <- function(x){
+    bad_names <- c("", "TOTAL", "NYS DOCCS", "NYS DOCCS INCARCERATED")
+    
+    ny <- as.data.frame(x) %>%
+        filter(!(X0 %in% bad_names))
+    
+    
+    col_name_mat <- matrix(c(
+            "FACILITY", "X0", "Name",
+            "RECOVERED", "X1", "Residents.Recovered",
+            "DECEASED", "X2", "Residents.Deaths",
+            "TOTAL POSITIVE", "X3", "Residents.Confirmed",
+            "PENDING", "X4", "Residents.Pending",
+            "NEGATIVE", "X5", "Residents.Negative"
+            ), ncol = 3, nrow = 6, byrow = TRUE)
+        
+    colnames(col_name_mat) <- c("check", "raw", "clean")
+    col_name_df <- as_tibble(col_name_mat)
+        
+    check_names_extractable(ny, col_name_df)
+        
+    rename_extractable(ny, col_name_df) %>%
+        filter(Name != "FACILITY") %>%
+        clean_scraped_df() %>%
+        as_tibble() %>%
+        mutate(Name = ifelse(
+            grepl(Name, pattern = "MOHAWK"), "MOHAWK WALSH RMU", Name))
+}
+
+#' Scraper class for general new_york COVID data
+#' 
+#' @name new_york_scraper
+#' @description NY facility level data is only provided for residents and not
+#' staff. Pdf has been consistent but data used to be directly embedded in the
+#' html.
+#' \describe{
+#'   \item{Facility}{The facility name}
+#'   \item{Recovered}{Residents recovered from infection}
+#'   \item{Deceased}{Residents deaths related to covid}
+#'   \item{Positive}{Residents Confirmed}
+#'   \item{Pending}{Residents Pending}
+#'   \item{Negative}{Residents Negative}
+#' }
+
+new_york_scraper <- R6Class(
+    "new_york_scraper",
+    inherit = generic_scraper,
+    public = list(
+        log = NULL,
+        initialize = function(
+            log,
+            url = "https://doccs.ny.gov/doccs-covid-19-report",
+            id = "new_york",
+            type = "pdf",
+            state = "NY",
+            # pull the JSON data directly from the API
+            pull_func = new_york_pull,
+            # restructuring the data means pulling out the data portion of the json
+            restruct_func = new_york_restruct,
+            # Rename the columns to appropriate database names
+            extract_func = new_york_extract){
+            super$initialize(
+                url = url, id = id, pull_func = pull_func, type = type,
+                restruct_func = restruct_func, extract_func = extract_func,
+                log = log, state = state)
+        }
+    )
+)
+
+if(sys.nframe() == 0){
+    new_york <- new_york_scraper$new(log=TRUE)
+    new_york$raw_data
+    new_york$pull_raw()
+    new_york$raw_data
+    new_york$save_raw()
+    new_york$restruct_raw()
+    new_york$restruct_data
+    new_york$extract_from_raw()
+    new_york$extract_data
+    new_york$validate_extract()
+    new_york$save_extract()
+}
+
---
+++ production/scrapers/new_york_statewide.R
@@ -0,0 +1,88 @@
+source("./R/generic_scraper.R")
+source("./R/utilities.R")
+
+new_york_statewide_pull <- function(x){
+    sp <- start_splash()
+    splashr::render_html(splash_obj = sp, url = x)
+}
+
+new_york_statewide_restruct <- function(x){
+    tables <- x %>%
+        rvest::html_nodes("table")
+    
+    is_confirmed <- tables[[1]] %>%
+        rvest::html_nodes("h5") %>%
+        rvest::html_text() %>%
+        str_detect("(?i)confirmed")
+    
+    if(!is_confirmed){
+        warning("Table structure may have changed")
+    }
+    
+    tables[[1]] %>%
+        rvest::html_table() %>%
+        select(Staff.Confirmed = Staff) %>%
+        mutate(Name = "State-Wide") %>%
+        mutate(
+            Staff.Deaths = x %>%
+                rvest::html_node(
+                    xpath="//h5[contains(text(),'DEATH')]/following::table") %>%
+                   rvest::html_table() %>%
+                   pull(Staff))
+}
+
+new_york_statewide_extract <- function(x){
+    x %>%
+        clean_scraped_df() %>%
+        as_tibble()
+}
+
+#' Scraper class for general NY statewide COVID data
+#' 
+#' @name new_york_statewide_scraper
+#' @description NY only produces staff data at the state level within the
+#' html of the site. Data is scrapped from tables.
+#' \describe{
+#'   \item{Staff Deaths}{Staff Deaths}
+#'   \tem{Staff Confirmed}{Staff Confirmed}
+#' }
+
+new_york_statewide_scraper <- R6Class(
+    "new_york_statewide_scraper",
+    inherit = generic_scraper,
+    public = list(
+        log = NULL,
+        initialize = function(
+            log,
+            url = "https://doccs.ny.gov/doccs-covid-19-report",
+            id = "new_york_statewide",
+            type = "html",
+            state = "NY",
+            # pull the JSON data directly from the API
+            pull_func = new_york_statewide_pull,
+            # restructuring the data means pulling out the data portion of the json
+            restruct_func = new_york_statewide_restruct,
+            # Rename the columns to appropriate database names
+            extract_func = new_york_statewide_extract){
+            super$initialize(
+                url = url, id = id, pull_func = pull_func, type = type,
+                restruct_func = restruct_func, extract_func = extract_func,
+                log = log, state = state)
+        }
+    )
+)
+
+if(sys.nframe() == 0){
+    new_york_statewide <- new_york_statewide_scraper$new(log=TRUE)
+    new_york_statewide$raw_data
+    new_york_statewide$pull_raw()
+    new_york_statewide$raw_data
+    new_york_statewide$save_raw()
+    new_york_statewide$restruct_raw()
+    new_york_statewide$restruct_data
+    new_york_statewide$extract_from_raw()
+    new_york_statewide$extract_data
+    new_york_statewide$validate_extract()
+    new_york_statewide$save_extract()
+}
+
