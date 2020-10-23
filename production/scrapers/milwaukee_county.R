a77f122d0e3519fa08563f1a7cc9ddceaa576ccd
Neal Marquez <nmmarquez@gmail.com>
2020-10-23 10:10:31 -0400

    feat: polk and milwaukee scrapers

---
+++ production/scrapers/milwaukee_county.R
@@ -0,0 +1,92 @@
+source("./R/generic_scraper.R")
+source("./R/utilities.R")
+
+milwaukee_county_pull <- function(x){
+    get_src_by_attr(x, "img", attr = "src", attr_regex = "HOCdashboard.png") %>%
+        magick::image_read()
+}
+
+milwaukee_county_restruct <- function(x){
+    x %>%
+        magick::image_crop("800x120+0+437") %>%
+        ExtractTable()
+}
+
+milwaukee_county_extract <- function(x){
+    df_ <- as.data.frame(t(x[[1]]))
+
+    col_name <- matrix(c(
+        "Name", "0", "",
+        "Drop.Residents.Active", "1", "Current Cases",
+        "Residents.Pending", "2", "Pending Tests",
+        "Residents.Negative", "3", "Negative Tests",
+        "Drop.Residents.Hospital", "4", "Hospitalized",
+        "Residents.Deaths", "5", "Deaths"
+    ), ncol = 3, nrow = 6, byrow = TRUE)
+    
+    colnames(col_name) <- c("clean", "raw", "check")
+    col_name_df <- as_tibble(col_name)
+    check_names_extractable(df_, col_name_df)
+    
+    rename_extractable(df_, col_name_df) %>%
+        filter(Name != "") %>%
+        clean_scraped_df() %>%
+        as_tibble() %>%
+        mutate_if(is.numeric, function(x) ifelse(is.na(x), 0, x)) %>%
+        select(-starts_with("Drop"))
+}
+
+#' Scraper class for general Milwaukee county COVID data
+#' 
+#' @name milwaukee_county_scraper
+#' @description Milwaukee reports resident data in an image hosted on the
+#' website. No information is given about total administered tests or the
+#' cumulative number of confirmed cases.
+#' \describe{
+#'   \item{Facility_Name}{The facility name}
+#'   \item{Current Cases}{Residents with active infections}
+#'   \item{Pending Tests}{Residents with pending tests}
+#'   \item{Negative Tests}{Residents with negative tests}
+#'   \item{Hospitalized}{Residents hospitalized}
+#'   \item{Deaths}{Resident deaths}
+#' }
+
+milwaukee_county_scraper <- R6Class(
+    "milwaukee_county_scraper",
+    inherit = generic_scraper,
+    public = list(
+        log = NULL,
+        initialize = function(
+            log,
+            url = "https://county.milwaukee.gov/EN/COVID-19/Individuals-in-County-Care",
+            id = "milwaukee_county",
+            type = "img",
+            state = "MN",
+            # pull the JSON data directly from the API
+            pull_func = milwaukee_county_pull,
+            # restructuring the data means pulling out the data portion of the json
+            restruct_func = milwaukee_county_restruct,
+            # Rename the columns to appropriate database names
+            extract_func = milwaukee_county_extract){
+            super$initialize(
+                url = url, id = id, pull_func = pull_func, type = type,
+                restruct_func = restruct_func, extract_func = extract_func,
+                log = log, state = state)
+        }
+    )
+)
+
+if(sys.nframe() == 0){
+    milwaukee_county <- milwaukee_county_scraper$new(log=TRUE)
+    milwaukee_county$raw_data
+    milwaukee_county$pull_raw()
+    milwaukee_county$raw_data
+    milwaukee_county$save_raw()
+    milwaukee_county$restruct_raw()
+    milwaukee_county$restruct_data
+    milwaukee_county$extract_from_raw()
+    milwaukee_county$extract_data
+    milwaukee_county$validate_extract()
+    milwaukee_county$save_extract()
+}
+
---
+++ production/scrapers/polk_county.R
@@ -0,0 +1,87 @@
+source("./R/generic_scraper.R")
+source("./R/utilities.R")
+
+polk_county_pull <- function(x){
+    get_src_by_attr(x, "img", attr = "src", attr_regex = "HOCdashboard.png") %>%
+        magick::image_read()
+}
+
+polk_county_restruct <- function(x){
+    ExtractTable(x)
+}
+
+polk_county_extract <- function(x){
+    
+    col_name <- matrix(c(
+        "Residents.Tested", "0", "Inmate Tests Administered",
+        "Residents.Confirmed", "1", "Inmates Positive",
+        "Staff.Tested", "2", "Staff Tests Administered",
+        "Staff.Confirmed", "3", "Staff Positive"
+        ), ncol = 3, nrow = 4, byrow = TRUE)
+    
+    colnames(col_name) <- c("clean", "raw", "check")
+    col_name_df <- as_tibble(col_name)
+    
+    df_ <- as.data.frame(x[[1]])
+    
+    check_names_extractable(df_, col_name_df)
+    
+    rename_extractable(df_, col_name_df) %>%
+        filter(!str_detect(Residents.Tested, "(?i)inmate")) %>%
+        mutate(Name = "Polk County Jail") %>%
+        clean_scraped_df() %>%
+        as_tibble()
+}
+
+#' Scraper class for general Polk county COVID data
+#' 
+#' @name polk_county_scraper
+#' @description Polk county reports testing and confirmed data for staff and
+#' residents in an image hosted on the web page. Test number appear to be tests
+#' administered not individuals tested.
+#' \describe{
+#'   \item{Inmate Tests Administered}{Number of tests administered to residents}
+#'   \item{Inmates Positive}{Number of Inmates Confirmed}
+#'   \item{Staff Tests Administered}{Number of tests administered to staff}
+#'   \item{Staff Positive}{Number of staff confirmed}
+#' }
+
+polk_county_scraper <- R6Class(
+    "polk_county_scraper",
+    inherit = generic_scraper,
+    public = list(
+        log = NULL,
+        initialize = function(
+            log,
+            url = "https://www.polkcountyiowa.gov/county-sheriff/news-press-releases/covid-19-quick-stats/",
+            id = "polk_county",
+            type = "img",
+            state = "IA",
+            # pull the JSON data directly from the API
+            pull_func = polk_county_pull,
+            # restructuring the data means pulling out the data portion of the json
+            restruct_func = polk_county_restruct,
+            # Rename the columns to appropriate database names
+            extract_func = polk_county_extract){
+            super$initialize(
+                url = url, id = id, pull_func = pull_func, type = type,
+                restruct_func = restruct_func, extract_func = extract_func,
+                log = log, state = state)
+        }
+    )
+)
+
+if(sys.nframe() == 0){
+    polk_county <- polk_county_scraper$new(log=TRUE)
+    polk_county$raw_data
+    polk_county$pull_raw()
+    polk_county$raw_data
+    polk_county$save_raw()
+    polk_county$restruct_raw()
+    polk_county$restruct_data
+    polk_county$extract_from_raw()
+    polk_county$extract_data
+    polk_county$validate_extract()
+    polk_county$save_extract()
+}
+
