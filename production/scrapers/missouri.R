4f9c2d539511d8e3e9b0443d5b098ad667cad09a
Neal Marquez <nmmarquez@gmail.com>
2020-11-09 20:46:54 -0500

    fix: MO new scraper code

--- production/scrapers/missouri.R
+++ production/scrapers/missouri.R
@@ -2,52 +2,69 @@ source("./R/generic_scraper.R")
 source("./R/utilities.R")
 
 missouri_pull <- function(x){
-    get_src_by_attr(x, "img", attr="src", attr_regex = "(?i)covid-chart") %>%
-        magick::image_read()
+    bi_url <- "https://results.mo.gov/t/DOC/views/CovidDashboard_Public/" %>%
+        str_c("CasesTable?%3AisGuestRedirectFromVizportal=y&%3Aembed=y")
+
+    remDr <- RSelenium::remoteDriver(
+        remoteServerAddr = "localhost",
+        port = 4445,
+        browserName = "firefox",
+        extraCapabilities=fprof
+    )
+    
+    del_ <- capture.output(remDr$open())
+    remDr$navigate(bi_url)
+    Sys.sleep(6)
+
+    old_windows <- unlist(remDr$getWindowHandles())
+    
+    remDr$findElement(
+        "css", "[id='download-ToolbarButton']")$clickElement()
+    Sys.sleep(10)
+    remDr$findElement(
+        "css", "[data-tb-test-id='DownloadData-Button']")$clickElement()
+    Sys.sleep(10)
+    
+    new_window <- setdiff(unlist(remDr$getWindowHandles()), old_windows)
+    remDr$switchToWindow(new_window)
+    Sys.sleep(10)
+    
+    x = xml2::read_html(remDr$getPageSource()[[1]])
 }
 
 missouri_restruct <- function(x){
-    ExtractTable(x)
-}
-
-missouri_extract <- function(x){
-    col_name_mat <- matrix(c(
-        "Facility", "0", "Name",
-        "Staff Active Cases", "1", "Drop.Staff.Active",
-        "Staff Recovered", "2", "Staff.Recovered",
-        "Staff Cumulative Cases", "3", "Staff.Confirmed",
-        "Offender Active Cases", "4", "Drop.Residents.Active",
-        "Offenders Recovered", "5", "Residents.Recovered",
-        "Offender Cumulative Cases", "6", "Residents.Confirmed"
-        ), ncol = 3, nrow = 7, byrow = TRUE)
-    
-    colnames(col_name_mat) <- c("check", "raw", "clean")
-    col_name_df <- as_tibble(col_name_mat)
-    
-    check_names_extractable(x[[1]], col_name_df)
-    
-    rename_extractable(x[[1]], col_name_df) %>%
-        filter(Name!="Facility") %>%
-        select(-starts_with("Drop")) %>%
-        clean_scraped_df() %>%
+    x %>%
+        rvest::html_node("table") %>%
+        rvest::html_table() %>%
         as_tibble()
 }
 
-#' Scraper class for general missouri COVID data
+missouri_extract <- function(x){
+    x %>%
+        mutate(`Recovery Status` = str_remove(
+            `Recovery Status`, " Positive")) %>%
+        mutate(Designation = str_replace(
+            Designation, "Offender", "Residents")) %>%
+        mutate(col_name = str_c(Designation, ".", `Recovery Status`)) %>%
+        select(-Designation, -`Recovery Status`) %>%
+        pivot_wider(
+            names_from = col_name, values_from = `AGG(CountWithZero)`) %>%
+        rename(Name = Location) %>%
+        select(-Staff.Active)
+}
+
+#' Scraper class for general Missouri COVID data
 #' 
 #' @name missouri_scraper
-#' @description Facility specific MO data comes from an image hosted on the
-#' page. Detailed information such as the number of deaths and the number
-#' tested are omitted and only reported at the state level. See the
-#' missouri_statewide scraper for more info.
+#' @description Facility specific MO data comes from a power bi html table. 
+#' Cumulative cases for facilities and deaths at the state level appear to
+#' be no longer reported here.
 #' \describe{
 #'   \item{Facility}{The facility name}
 #'   \item{Staff Active Cases}{Current infections among staff}
 #'   \item{Staff Recovered}{Staff who have recovered from infection}
-#'   \item{Staff Cumulative Cases}{Cumulative case count among staff}
 #'   \item{Offender Active Cases}{Current infections among residents}
 #'   \item{Offenders Recovered}{Residents who have recovered from infection}
-#'   \item{Offender Cumulative Cases}{cumulative case count among residents}
 #' }
 
 missouri_scraper <- R6Class(
@@ -57,9 +74,9 @@ missouri_scraper <- R6Class(
         log = NULL,
         initialize = function(
             log,
-            url = "https://doc.mo.gov/media-center/newsroom/covid-19/data",
+            url = "https://doc.mo.gov/media-center/newsroom/covid-19",
             id = "missouri",
-            type = "img",
+            type = "html",
             state = "MO",
             # pull the JSON data directly from the API
             pull_func = missouri_pull,
