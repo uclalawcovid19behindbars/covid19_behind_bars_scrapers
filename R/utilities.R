f85708407a891f5cf74548616b412b444aae1d9c
Neal Marquez <nmmarquez@gmail.com>
2020-11-13 19:35:39 -0500

    update utils

--- R/utilities.R
+++ R/utilities.R
@@ -614,6 +614,37 @@ vector_na_sum <- function(...){
     })
 }
 
+#' A re-coding of the coalesce function to include warnings when multiple
+#' values are given which are not NA and are different
+#' 
+#' @param ... vectors of equal length and type to coalesce
+#' @return vector of coalesced values
+#' 
+#' @examples 
+#' coalesce_with_warnings(1:3, 4:6)
+#' coalesce_with_warnings(1:3, c(1:2, NA))
+
+coalesce_with_warnings <- function(...){
+    d <- cbind(...)
+    
+    sapply(1:nrow(d), function(i){
+        x <- d[i,]
+        if(all(is.na(x))){
+            out <- NA
+        }
+        else{
+            xbar <- unique(as.vector(na.omit(x)))
+            if(length(xbar) != 1){
+                warning(paste0(
+                  "Row ", i, " has multiple values that do not match."))
+            }
+            # only grab the first one
+            out <- xbar[1]
+        }
+    out
+    })
+}
+
 load_latest_data <- function(){
   
     scrapers <- str_remove(list.files("./production/scrapers"), ".R")
