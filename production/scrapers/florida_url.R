738dbb22a6dc9ef18fbfeeca83dd5ab4c62f277d
Neal Marquez <nmmarquez@gmail.com>
2020-12-20 12:28:10 -0500

    fix: move florida scrape location

--- production/scrapers/florida_url.R
+++ production/scrapers/florida_url.R
@@ -1,55 +1,88 @@
 source("./R/generic_scraper.R")
 source("./R/utilities.R")
 
-florida_url_restruct <- function(x){
-    fl <- x %>%
-        rvest::html_node('table') %>%
-        rvest::html_table()
+florida_url_pull <- function(x){
+    html_page <- xml2::read_html(x)
     
-    expected <- c(
-        "Facility", "Security Quarantine", "Medical Quarantine", 
-        "Medical Isolation", "Pending Tests", "Negative Tests", 
-        "Positive Tests", "Positive Tests")
-    received <- unname(unlist(fl[1,]))
+    data_imgs <- html_page %>%
+        rvest::html_nodes("ul") %>%
+        .[[2]] %>%
+        rvest::html_nodes("img") %>%
+        rvest::html_attr("src") %>%
+        xml2::url_absolute(x)
     
-    basic_check(received, expected)
+    magick::image_append(
+        c(
+            magick::image_read(data_imgs[1]),
+            magick::image_read(data_imgs[2]),
+            magick::image_read(data_imgs[3]),
+            magick::image_read(data_imgs[4]),
+            magick::image_read(data_imgs[5])
+        ), stack = TRUE)
     
-    names(fl)[1] <- "Name"
-    names(fl)[2] <- "Quar1"
-    names(fl)[3] <- "Quar2"
-    names(fl)[4] <- "Quar3"
-    names(fl)[5] <- "Residents.Pending"
-    names(fl)[6] <- "Residents.Negative"
-    names(fl)[7] <- "Residents.Confirmed"
-    names(fl)[8] <- "Staff.Confirmed"
-        
-    state_death <- x %>%
-        rvest::html_nodes('.smallTable') %>%
-        .[[3]] %>%
-        rvest::html_table()
+}
+
+process_FL_cell <- function(x, crop, enhancement = 500, debug = FALSE){
     
-    if(!any(grepl("Inmate Deaths", names(state_death)))){
-        warning(str_c(
-            "Attempted to grab state deaths but column may not be correct."))
+    sub_img <- x %>%
+        # crop out a specific cell
+        magick::image_crop(crop)
+    
+    if(debug){
+        return(sub_img)
     }
     
-    bind_rows(
-        fl,
-        tibble(
-            Name = "State-Wide",
-            Residents.Deaths = state_death[,2]))
+    suppressWarnings(out_ <- sub_img %>%
+                         # convert to gray-scale
+                         magick::image_convert(type = 'Grayscale') %>%
+                         # enhance image
+                         magick::image_resize(paste(enhancement, "x")) %>%
+                         magick::image_ocr() %>%
+                         stringr::str_remove_all(",|\\n") %>%
+                         as.numeric() %>%
+                         # sometimes when extraction fails it spits a
+                         # nonsensical negative number
+                         {ifelse(. < 0, NA, .)})
+    
+    return(out_)
+}
+
+process_FL_image <- function(base_image, ...){
+    tibble(
+        Residents.Confirmed = process_FL_cell(x, "300x42+0+300", ...),
+        Staff.Confirmed = process_FL_cell(x, "300x42+0+430", ...),
+        Residents.Recovered = process_FL_cell(x, "300x42+700+300", ...),
+        Staff.Recovered = process_FL_cell(x, "300x42+700+430", ...),
+        Residents.Deaths = process_FL_cell(x, "400x105+0+545", ...)
+    )
+}
+
+florida_url_restruct <-  function(x, enhancements = seq(100, 500, by = 50)){
+    base_image <- x
+    
+    dat_df <- bind_rows(lapply(enhancements, function(e){
+        process_FL_image(base_image, enhancement = e)})) %>%
+        # we should write a test here if more than one value is returned as right
+        # now the function will just return NA if there is more than one value
+        # or there is no values extracted
+        summarize_all(function(x){
+            ifelse(length(unique(na.omit(x))) == 1, unique(na.omit(x)), NA)
+        })
+    
+    for(i in names(dat_df)){
+        if(any(is.na(dat_df[[i]]))){
+            warning(paste0(
+                "NA value extracted tho not expected for FL column ", i))
+        }
+    }
+    
+    dat_df
 }
 
 florida_url_extract <- function(x){
     x %>%
-        mutate(Name = str_remove_all(Name, "Operated by.*")) %>%
-        filter(Name!= "Facility" & Name!= "Totals") %>%
-        clean_scraped_df() %>%
-        mutate(Residents.Quarantine = Quar1 + Quar2 + Quar3) %>%
-        select(-Quar1, -Quar2, -Quar3) %>%
-        as_tibble() %>%
-        mutate(Residents.Tadmin = Residents.Pending +
-                   Residents.Negative + Residents.Confirmed)
+        mutate(Name = "State-Wide") %>%
+        clean_scraped_df()
 }
 
 #' Scraper class for general Florida COVID data
@@ -79,11 +112,11 @@ florida_url_scraper <- R6Class(
             log,
             url = "http://www.dc.state.fl.us/comm/covid-19.html",
             id = "florida_url",
-            type = "html",
+            type = "img",
             state = "FL",
             jurisdiction = "state",
             # pull the JSON data directly from the API
-            pull_func = xml2::read_html,
+            pull_func = florida_url_pull,
             restruct_func = florida_url_restruct,
             # Rename the columns to appropriate database names
             extract_func = florida_url_extract){
