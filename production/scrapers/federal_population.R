4097be57cdc73703b1034e4e7276fced5c28cc10
erika-tyagi <erika.tyagi@gmail.com>
2020-12-28 11:55:42 -0600

    Adding federal population scraper

---
+++ production/scrapers/federal_population.R
@@ -0,0 +1,80 @@
+source("./R/generic_scraper.R")
+source("./R/utilities.R")
+
+federal_population_pull <- function(x) {
+    "https://www.bop.gov/PublicInfo/execute/popreport?todo=query&output=json" %>% 
+    jsonlite::read_json(simplifyVector = TRUE)
+}
+
+federal_population_restruct <- function(x) {
+    bind_rows(
+        x$BOP, 
+        x$PRIVATE, 
+        x$CCM
+    ) %>% as_tibble()
+}
+
+federal_population_extract <- function(x) {
+    
+    df_ <- x 
+    
+    exp_names <- c(
+        Drop.code = "code", 
+        Name = "Name", 
+        Drop.state = "state", 
+        Residents.Population = "popCount", 
+        Drop.groupDescription = "groupDescription", 
+        Drop.sortKey = "sortKey", 
+        Drop.sortNameKey = "sortNameKey", 
+        Drop.sortpzapSequence = "sortpzapSequence", 
+        Drop.indentationIndicator = "indentationIndicator", 
+        Drop.recordtype = "recordtype"
+    )
+    
+    check_names(df_, exp_names)
+    names(df_) <- names(exp_names)
+    
+    df_ %>%
+        select(-starts_with("Drop")) %>%
+        mutate(Name = clean_fac_col_txt(Name, to_upper = TRUE))
+}
+
+federal_population_scraper <- R6Class(
+    "federal_population_scraper",
+    inherit = generic_scraper,
+    public = list(
+        log = NULL,
+        initialize = function(
+            log,
+            url = "https://www.bop.gov/mobile/about/population_statistics.jsp",
+            id = "federal_population",
+            type = "json",
+            state = "federal",
+            jurisdiction = "federal",
+            # pull the JSON data directly from the API
+            pull_func = federal_population_pull,
+            # restructuring the data means pulling out the data portion of the json
+            restruct_func = federal_population_restruct,
+            # Rename the columns to appropriate database names
+            extract_func = federal_population_extract){
+            super$initialize(
+                url = url, id = id, pull_func = pull_func, type = type,
+                restruct_func = restruct_func, extract_func = extract_func,
+                log = log, state = state, jurisdiction = jurisdiction)
+        }
+    )
+)
+
+if(sys.nframe() == 0){
+    federal_population <- federal_population_scraper$new(log=TRUE)
+    federal_population$raw_data
+    federal_population$pull_raw()
+    federal_population$raw_data
+    federal_population$save_raw()
+    federal_population$restruct_raw()
+    federal_population$restruct_data
+    federal_population$extract_from_raw()
+    federal_population$extract_data
+    federal_population$validate_extract()
+    federal_population$save_extract()
+}
