189b55a798aea759ea5e14b4689d0b7936d55da1
Neal Marquez <nmmarquez@gmail.com>
2021-02-09 10:07:32 -0500

    fix: version control summarize cron job

---
+++ prototyping/summarize.R
@@ -0,0 +1,27 @@
+#!/usr/bin/Rscript
+
+library(tidyverse)
+
+ind_vars <- c("Date", "Name", "State")
+    
+list.files("/srv/shiny-server/scraper_data/extracted_data", full.names = TRUE) %>%
+    lapply(function(x){
+        df_ <- read_csv(x, col_types = cols())
+        if("Date" %in% names(df_)){
+            df_ <- df_ %>%
+                mutate(Date = lubridate::as_date(Date)) %>%
+		mutate_at(vars(starts_with("Residents")), as.numeric) %>%
+		mutate_at(vars(starts_with("Staff")), as.numeric)
+        }
+        df_
+    }) %>%
+    bind_rows() %>%
+    select(-Resident.Deaths) %>%
+    # remove values if they are missing a data name or state
+    filter(!is.na(Date) & !is.na(Name) & State != "") %>%
+    # order the names alphabetically
+    select(!!sort(names(.))) %>%
+    # put the indicator variables first
+    select(!!ind_vars, !!(names(.)[!(names(.) %in% ind_vars)])) %>%
+    filter(!is.na(id), !is.na(jurisdiction)) %>%
+    write_csv("/srv/shiny-server/scraper_data/summary_data/aggregated_data.csv")
