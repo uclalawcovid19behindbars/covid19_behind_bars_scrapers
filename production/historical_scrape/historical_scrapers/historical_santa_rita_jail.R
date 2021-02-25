e0c27842f29527dcc17af53e624f543eddf65d99
hjohns12 <hopecaneel@gmail.com>
2021-02-25 11:40:08 -0500

    chore: another attempt, close but not quite

--- production/historical_scrape/historical_scrapers/historical_santa_rita_jail.R
+++ production/historical_scrape/historical_scrapers/historical_santa_rita_jail.R
@@ -1,21 +1,23 @@
 source("./R/generic_scraper.R")
 source("./R/utilities.R")
 
-santa_rita_jail_pull <- function(x){
+historical_santa_rita_jail_pull <- function(x){
     "196jMpPfuE4IMlplsd7K_3mP1l018cIbS-oTO2SuVklw" %>%
         googlesheets4::read_sheet()
 }
 
-santa_rita_jail_restruct <- function(x){
+historical_santa_rita_jail_restruct <- function(x, date = NULL){
+    if(date > lubridate::ymd("2020-12-28")){
+        stop(
+            "historical_santa_rita_jail should not be run past 2020-12-28 as to",
+            "not overlap", "with santa_rita_jail scraper")
+    }
     x %>%
         filter(!is.na(Date)) %>%
-        mutate(Date = lubridate::ymd(Date)) %>%
-        filter(Date == max(Date))
+        mutate(Date = lubridate::ymd(Date)) 
 }
 
-santa_rita_jail_extract <- function(x, exp_date = Sys.Date()){
-    
-    error_on_date(x$Date, exp_date)
+historical_santa_rita_jail_extract <- function(x, date){
     
     check_names(x, c(
         "Date", 
@@ -59,36 +61,38 @@ santa_rita_jail_extract <- function(x, exp_date = Sys.Date()){
             Residents.Population = `SRJ Population (total)`,
             Staff.Confirmed = `Staff cases (total)`
         ) %>%
-        mutate(Name = "SANTA RITA JAIL")
+        mutate(Name = "SANTA RITA JAIL") %>%
+        filter(Date == date) %>%
+        select(-Date)
 }
 
-#' Scraper class for general santa_rita_jail COVID data
+#' Scraper class for general historical_santa_rita_jail COVID data
 #' 
-#' @name santa_rita_jail_scraper
-#' @description This will be a description of santa_rita_jail data and what the scraper
+#' @name historical_santa_rita_jail_scraper
+#' @description This will be a description of historical_santa_rita_jail data and what the scraper
 #' does
 #' \describe{
 #'   \item{Facility_Name}{The faciilty name.}
 #' }
 
-santa_rita_jail_scraper <- R6Class(
-    "santa_rita_jail_scraper",
+historical_santa_rita_jail_scraper <- R6Class(
+    "historical_santa_rita_jail_scraper",
     inherit = generic_scraper,
     public = list(
         log = NULL,
         initialize = function(
             log,
             url = "https://www.davisvanguard.org/tag/covid-19/",
-            id = "santa_rita_jail",
+            id = "historical_santa_rita_jail",
             type = "csv",
             state = "CA",
             jurisdiction = "county",
             # pull the JSON data directly from the API
-            pull_func = santa_rita_jail_pull,
+            pull_func = historical_santa_rita_jail_pull,
             # restructuring the data means pulling out the data portion of the json
-            restruct_func = santa_rita_jail_restruct,
+            restruct_func = historical_santa_rita_jail_restruct,
             # Rename the columns to appropriate database names
-            extract_func = santa_rita_jail_extract){
+            extract_func = historical_santa_rita_jail_extract){
             super$initialize(
                 url = url, id = id, pull_func = pull_func, type = type,
                 restruct_func = restruct_func, extract_func = extract_func,
@@ -98,16 +102,17 @@ santa_rita_jail_scraper <- R6Class(
 )
 
 if(sys.nframe() == 0){
-    santa_rita_jail <- santa_rita_jail_scraper$new(log=TRUE)
-    santa_rita_jail$raw_data
-    santa_rita_jail$pull_raw()
-    santa_rita_jail$raw_data
-    santa_rita_jail$save_raw()
-    santa_rita_jail$restruct_raw()
-    santa_rita_jail$restruct_data
-    santa_rita_jail$extract_from_raw()
-    santa_rita_jail$extract_data
-    santa_rita_jail$validate_extract()
-    santa_rita_jail$save_extract()
+    historical_santa_rita_jail <- historical_santa_rita_jail_scraper$new(log=TRUE)
+    historical_santa_rita_jail$reset_date("SET_DATE_HERE")
+    historical_santa_rita_jail$raw_data
+    historical_santa_rita_jail$pull_raw(date = scraper$date, file = NULL, .dated_pull = TRUE)
+    historical_santa_rita_jail$raw_data
+    historical_santa_rita_jail$save_raw()
+    historical_santa_rita_jail$restruct_raw(date = historical_santa_rita$date)
+    historical_santa_rita_jail$restruct_data
+    historical_santa_rita_jail$extract_from_raw(date = historical_santa_rita$date)
+    historical_santa_rita_jail$extract_data
+    historical_santa_rita_jail$validate_extract()
+    historical_santa_rita_jail$save_extract()
 }
 
