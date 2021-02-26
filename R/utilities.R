d60d8460bcaa3b95cd498aa8dbe9585f2c76e12c
nmmarquez <nmmarquez@gmail.com>
2021-02-26 10:33:18 -0500

    Write vacc agg (#131)
    * update: add vaccines to our data rrepo
    
    * update: fix diagnostics bug
    
    * feat: add in all vars not just vaccine vars
    
    * fix: should change the file path for state aggs now
    
    * Made NAs empty strings
    
    Co-authored-by: erika-tyagi <erika.tyagi@gmail.com>

--- R/utilities.R
+++ R/utilities.R
@@ -565,6 +565,16 @@ write_latest_data <- function(coalesce = TRUE, fill = FALSE){
             Longitude, County.FIPS, HIFLD.ID, ICE.Field.Office) %>% 
         write_csv("./data/latest-data/adult_facility_covid_counts.csv", 
                   na="")
+
+    full_var_df <- behindbarstools::calc_aggregate_counts(state = TRUE)
+
+    full_var_df %>%
+        filter(!is.na(Val)) %>%
+        select(State, Measure, Val) %>%
+        pivot_wider(names_from = "Measure", values_from = "Val") %>%
+        arrange(State) %>%
+        write_csv("./data/latest-data/state_aggregate_counts.csv", 
+                  na="")
 }
 
 get_latest_manual <- function(state){
@@ -637,7 +647,7 @@ hist_config_update <- function(df){
 
 write_agg_data <- function(...){
     raw_agg <- calc_aggregate_counts(...)
-  
+
     # the order and selection of cols corresponds to values in the Google sheet
     # https://docs.google.com/spreadsheets/d/1MCiyyaz1PtQX_AZ5sMRUOIOttbi8nqIvXCcPt4j4RIo
     sel_vars <- c(
