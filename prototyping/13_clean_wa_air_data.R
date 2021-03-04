46fda7a31680f443512c0c3c2e28126f2ffd62e0
Neal Marquez <nmmarquez@gmail.com>
2021-03-04 18:40:48 -0500

    update: FL fix inconsistoncies

---
+++ prototyping/13_clean_wa_air_data.R
@@ -0,0 +1,38 @@
+rm(list=ls())
+library(tidyverse)
+library(lubridate)
+
+DRY_RUN <- TRUE
+
+wa_files <- list.files(
+    "/srv/shiny-server/scraper_data/extracted_data",
+    "\\d+-\\d+-\\d+_washington.csv", full.names = TRUE
+    )
+
+for(waf in wa_files){
+    wa_df <- read_csv(waf, col_types = cols())
+
+    if(all(wa_df$Date == ymd("2020-12-02"))){
+        print("Changing AIRWAY HEIGHTS value to 30 for date of 2020-12-02")
+        wa_df$Residents.Confirmed[
+            wa_df$Name == "Airway Heights Corrections Center"] <- 30
+    }
+
+    if(all(wa_df$Date > ymd("2020-12-02"))){
+        print(paste0(
+            "Changing AIRWAY HEIGHTS value to NA for date of ",
+            as.character(wa_df$Date[1])))
+        wa_df$Residents.Confirmed[
+            wa_df$Name == "Airway Heights Corrections Center"] <- NA
+    }
+
+    wa_df %>%
+        filter(str_starts(Name, "(?i)Airway")) %>%
+        select(Name, Date, Residents.Confirmed) %>%
+        print()
+
+    if(!DRY_RUN){
+        write_csv(wa_df, waf)
+    }
+
+}
