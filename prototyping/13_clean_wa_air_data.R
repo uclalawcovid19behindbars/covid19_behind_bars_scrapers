fa6f8e815be638dcbca8f1022105d909ba45679f
Neal Marquez <nmmarquez@gmail.com>
2021-03-04 19:30:07 -0500

    update: fl clean checks

--- prototyping/13_clean_wa_air_data.R
+++ prototyping/13_clean_wa_air_data.R
@@ -11,11 +11,14 @@ wa_files <- list.files(
 
 for(waf in wa_files){
     wa_df <- read_csv(waf, col_types = cols())
+    
+    change <- FALSE
 
     if(all(wa_df$Date == ymd("2020-12-02"))){
         print("Changing AIRWAY HEIGHTS value to 30 for date of 2020-12-02")
         wa_df$Residents.Confirmed[
             wa_df$Name == "Airway Heights Corrections Center"] <- 30
+        change <- TRUE
     }
 
     if(all(wa_df$Date > ymd("2020-12-02"))){
@@ -24,14 +27,17 @@ for(waf in wa_files){
             as.character(wa_df$Date[1])))
         wa_df$Residents.Confirmed[
             wa_df$Name == "Airway Heights Corrections Center"] <- NA
+        change <- TRUE
     }
 
-    wa_df %>%
-        filter(str_starts(Name, "(?i)Airway")) %>%
-        select(Name, Date, Residents.Confirmed) %>%
-        print()
+    if(change){
+        wa_df %>%
+            filter(str_starts(Name, "(?i)Airway")) %>%
+            select(Name, Date, Residents.Confirmed) %>%
+            print()
+    }
 
-    if(!DRY_RUN){
+    if(!DRY_RUN & change){
         write_csv(wa_df, waf)
     }
 
