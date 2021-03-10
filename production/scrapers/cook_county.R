1f6f9a6d82c70878a4c95f4d0088d7428d8d32cd
Neal Marquez <nmmarquez@gmail.com>
2021-03-10 12:59:26 -0500

    debug: cook county jails

--- production/scrapers/cook_county.R
+++ production/scrapers/cook_county.R
@@ -2,77 +2,99 @@ source("./R/generic_scraper.R")
 
 cook_county_restruct <- function(x){
     
-    Staff.Txt <- x %>%
-        rvest::html_nodes(., 'ul+ p') %>%
-        rvest::html_text()
-
-    Staff.Active1 <- Staff.Txt %>%
-        str_extract("[0-9]+ correctional officers are positive for COVID") %>%
-        str_remove_all("[^0-9]") %>%
-        as.numeric()
-
-    Staff.Active2 <- Staff.Txt %>%
-        str_extract("as are [0-9]+ other Cook County Sheriff") %>%
-        str_remove_all("[^0-9]") %>%
-        as.numeric()
+    # grab the main div which has covid related data
+    covid_section <- x %>%
+        rvest::html_nodes(xpath="//*[text()='COVID-19 Cases at CCDOC']/..") %>%
+        .[[which(rvest::html_name(.) == "div")]]
     
-    tibble(
-        Residents.Recovered = x %>%
-            rvest::html_nodes(., '.col-md-8 li:nth-child(2)') %>%
-            rvest::html_text() %>%
-            str_replace_all('(?<=\\d),(?=\\d)', '') %>%
-            str_extract("[0-9]+") %>%
-            as.numeric(),
+    # get the jail detainee information
+    covid_sub_text <- covid_section %>%
+        rvest::html_nodes("li") %>%
+        # hacky way to only get text from parent node
+        map_chr(~paste0(rvest::html_text(rvest::html_nodes(
+            ., xpath="./text()"), trim=TRUE), collapse=" ")) %>%
+        str_remove_all("(?i)covid-19") %>%
+        str_replace_all(",([0-9])", "\\1")
+    
+    # see where the text is
+    res_idx <- list(
+        Residents.Active = which(
+            str_detect(covid_sub_text,"(?i)currently positive") & 
+                str_detect(covid_sub_text, "(?i)detainees")),
         
-        Residents.Deaths = x %>%
-            rvest::html_nodes(., '.col-md-8 li:nth-child(4)') %>%
-            rvest::html_text() %>%
-            str_replace_all('(?<=\\d),(?=\\d)', '') %>%
-            str_extract("[0-9]+") %>%
-            as.numeric(),
+        Residents.Recovered = which(
+            str_detect(covid_sub_text,"(?i)no longer positive") & 
+                str_detect(covid_sub_text, "(?i)detainees")),
         
-        Residents.Active = x %>%
-            rvest::html_node(., 'p+ ul li:nth-child(1)') %>%
-            rvest::html_text() %>%
-            str_replace_all('(?<=\\d),(?=\\d)', '') %>%
-            str_extract("[0-9]+") %>%
-            as.numeric(),
+        Residents.Negative = which(
+            str_detect(covid_sub_text,"(?i)negative") & 
+                str_detect(covid_sub_text, "(?i)detainees")),
         
-        Residents.Negative = x %>%
-            rvest::html_nodes(., '.col-md-8 li:nth-child(3)') %>%
-            rvest::html_text() %>%
-            str_replace_all('(?<=\\d),(?=\\d)', '') %>%
-            str_extract("[0-9]*") %>%
-            as.numeric(),
+        Residents.Deaths = which(
+            str_detect(covid_sub_text,"(?i)die") & 
+                str_detect(covid_sub_text, "(?i)detainees")))
+    
+    # make sure we have 1 and only 1 entry for each concept
+    if(!all(sapply(res_idx, length) == 1)){
+        stop("Website structure is not as expected please inspect")
+    }
+    
+    # extract the numeric values
+    resident_df <- sapply(res_idx, function(x){
+        sum_na_rm(as.numeric(
+            str_extract(unlist(str_split(covid_sub_text[x], " ")), "[0-9]+")))
+    }) %>%
+        t() %>%
+        as_tibble()
+    
+    # get the staff data which is not part of a list but rather in the main text
+    staff_sub_text <- covid_section %>%
+        rvest::html_nodes("p") %>%
+        rvest::html_text() %>%
+        str_remove_all("(?i)covid-19") %>%
+        .[(which(str_starts(., "(?i)as of")) + 1):(
+            which(str_detect(., "(?i)note:")) - 1)] %>%
+        str_replace_all(",([0-9])", "\\1")
+    
+    # do some sanity checks
+    ## there should be three text sections
+    if(length(staff_sub_text) != 3){
+        stop("Website structure is not as expected please inspect")
+    }
+    
+    ## only the first two have to do with positive staff
+    if(any(str_detect(staff_sub_text, "positive") != c(TRUE, TRUE, FALSE))){
+        stop("Website structure is not as expected please inspect")
+    }
+    
+    ## extract the staff data
+    staff_df <- tibble(
+        Staff.Confirmed = sapply(staff_sub_text[1:2], function(x){
+            sum_na_rm(as.numeric(
+                str_extract(unlist(str_split(x, " ")), "[0-9]+")))}) %>%
+            sum(),
         
-        Staff.Active = Staff.Active1 + Staff.Active2,
+        Staff.Recovered = sapply(staff_sub_text[2], function(x){
+            sum_na_rm(as.numeric(
+                str_extract(unlist(str_split(x, " ")), "[0-9]+")))}) %>%
+            sum(),
         
-        Staff.Recovered = x %>%
-            rvest::html_nodes(., 'p:nth-child(12)') %>%
-            rvest::html_text() %>%
-            str_replace_all('(?<=\\d),(?=\\d)', '') %>%
-            gsub("[^0-9]", "", .) %>%
-            as.numeric(),
-        
-        Staff.Deaths = x %>%
-            rvest::html_nodes(., 'p:nth-child(13)') %>%
-            rvest::html_text() %>%
-            str_replace_all('(?<=\\d),(?=\\d)', '') %>%
-            str_remove_all("COVID-19") %>%
-            str_extract_all("[0-9]+") %>%
+        Staff.Deaths = staff_sub_text[3] %>%
+            str_split(" ") %>%
             unlist() %>%
+            str_extract("[0-9]+") %>%
             as.numeric() %>%
-            sum()
-        )
+            sum_na_rm()
+    )
+    
+    bind_cols(resident_df, staff_df)
+    
 }
 
 cook_county_extract <- function(x){
     x %>%
-        mutate(Residents.Confirmed = 
-                   Residents.Recovered + Residents.Deaths + Residents.Active) %>%
-        mutate(Staff.Confirmed = 
-                   Staff.Recovered + Staff.Deaths + Staff.Active) %>%
-        select(-Residents.Active, -Staff.Active) %>%
+        mutate(Residents.Confirmed = Residents.Recovered + 
+                   Residents.Deaths + Residents.Active) %>%
         mutate(Name = "Cook County Jail")
         
 }
