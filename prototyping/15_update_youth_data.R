c1fb9641955d98687318d010b2c55203fe4206a9
hjohns12 <hopecaneel@gmail.com>
2021-04-06 17:57:08 -0400

    chore: add more to youth-specific sheet creation

--- prototyping/15_update_youth_data.R
+++ prototyping/15_update_youth_data.R
@@ -5,56 +5,57 @@ library(googlesheets4)
 gs4_auth("ucla.law.covid.staff@gmail.com")
 
 
-fac_sheet <- "1AfqaEPZTMy1hMdnZC8UYMP_JTJrnDlLi_6uh8sxmQWM"
+youth_sheet <- "1AfqaEPZTMy1hMdnZC8UYMP_JTJrnDlLi_6uh8sxmQWM"
 
 # fac_sheet_df <- read_sheet(fac_sheet, sheet = "Facility TAB") %>%
 #     filter(!is.na(`Facility ID`)) %>%
 #     select(-starts_with("...") , -ends_with("(Detainees)")) %>%
 #     select(-ends_with("Staff)"), -Address, -State)
 
-fac_sheet_df <- behindbarstools::read_fac_info()
+fac_sheet_df <- behindbarstools::read_fac_info() %>%
+    rename(`Facility ID` = Facility.ID)
 
 all_dat <- read_scrape_data(all_dates = FALSE) 
 youth_df <- all_dat %>%
     filter(Age == "Juvenile")
 
-new_df <- youth_df %>%
+# check for any other youth facilities 
+other_youth <- all_dat %>% 
+    filter((str_detect(Name, "(?i)juvenile|youth")) & Age != "Juvenile")
+
+# bind together age-classified and name-searched
+all_youth <- youth_df %>%
+    bind_rows(other_youth)
+
+new_df <- all_youth %>%
     # save only the latest data
     group_by(Facility.ID) %>%
     filter(Date == max(Date)) %>%
     ungroup() %>%
     select(
-        `Facility ID` = Facility.ID, 
-         Name, Address, City, State,
-        `TOTAL Confirmed Cases - YOUTH` = Residents.Confirmed,
-        `ACTIVE Confirmed Cases - YOUTH` = Residents.Active,
+        `Facility ID` = Facility.ID,
+        Name, 
+        Address, 
+        City, 
+        State,
+        `Cumulative Confirmed Cases - YOUTH` = Residents.Confirmed,
+        `Active Confirmed Cases - YOUTH` = Residents.Active,
         `Confirmed Deaths - YOUTH` = Residents.Deaths,
+        `Cumulative Confirmed Cases - YOUTH STAFF` = Staff.Confirmed,
+        `Confirmed Deaths - YOUTH STAFF` = Staff.Deaths,
         Date
     ) %>%
-    left_join(fac_sheet_df, by = "Facility ID") %>%
-    select(-`Facility ID`)
-
-update_sheet <- bind_rows(
-    data.frame(V1 = c("", ""), Name = c("", "")),
-    as.data.frame(new_df)
-)
-
-sum_cols <- c(
-    "TOTAL Confirmed Cases - YOUTH",
-    "ACTIVE Confirmed Cases - YOUTH",
-    "Confirmed Deaths - YOUTH"
-)
-
-update_sheet$Name[2] <- "Total:"
-
-for(sc in sum_cols){
-    update_sheet[[sc]][2] <- sum_na_rm(update_sheet[[sc]])
-}
-
-names(update_sheet)[1] <- ""
+    left_join(fac_sheet_df, 
+              by = "Facility ID", 
+              suffix = c(".scrape", "")) %>%
+    select(-ends_with(".scrape"),
+           -ICE.Field.Office) %>%
+    mutate(Date = as.character(Date),
+           Name = str_to_title(Name)) %>% 
+    relocate(Date, State, Name, ends_with("YOUTH"), ends_with("STAFF")) 
 
 range_write(
-    data = update_sheet, 
-    ss = fac_sheet, 
+    data = new_df, 
+    ss = youth_sheet, 
     sheet = "main", 
     reformat = FALSE)
