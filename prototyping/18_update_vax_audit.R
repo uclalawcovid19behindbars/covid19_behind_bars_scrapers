eaf8c97e6b1e1e9885b8dcdafa2caea8dceffd52
hjohns12 <hopecaneel@gmail.com>
2021-10-29 11:01:40 -0400

    chore: add more to vax audit sheet

--- prototyping/18_update_vax_audit.R
+++ prototyping/18_update_vax_audit.R
@@ -2,12 +2,14 @@ rm(list = ls())
 
 library(tidyverse)
 library(behindbarstools)
+library(googlesheets4)
 
 # ------------------------------------------------------------------------------
 
 GITHUB_PATH <- "https://raw.githubusercontent.com/uclalawcovid19behindbars/"
 STATE_JUR_PATH <- "data/master/latest-data/latest_state_jurisdiction_counts.csv"
 STATE_PATH <- "data/master/latest-data/latest_state_counts.csv"
+SPREADSHEET_LOC <- "1EOHjJttK4OlAoy6sdQsZimgu7JfwC41-CqCm9nmRc4o/edit#gid=0"
 
 # Read raw data 
 state_jur_df <- read.csv(str_c(GITHUB_PATH, STATE_JUR_PATH))
@@ -41,6 +43,19 @@ vax_audit_df <- state_jur_df %>%
     pivot_wider(names_from = Measure, values_from = c(Val, Rate)) %>%
     rename_all(~stringr::str_replace(.,"Val_","")) %>%
     left_join(state_df_small, by = "State") %>%
-    bind_rows(federal_ice_rows)
+    bind_rows(federal_ice_rows) %>%
+    mutate(`Source` = "",
+           `Does website report resident rates?` = "",
+           `Does website report staff rates?` = "",
+           `Notes` = "",
+           `Volunteer` = "")
+
+# Write data to google sheet
+gs4_auth("ucla.law.covid.staff@gmail.com")
+range_write(
+    data = all_youth_out, 
+    ss = SPREADSHEET_LOC, 
+    sheet = "Sheet1", 
+    reformat = FALSE)
     
 
